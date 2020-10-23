534eeb3863b47f74bf1048a389db069b3c7985b1
Jolie Ip <ipjolie1507@gmail.com>
2020-10-23 03:37:02 -0700

    first commit

--- appi.py
+++ appi.py
@@ -5,13 +5,71 @@ import plotly.express as px
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
-import plotly.graph_objects as plot
+import plotly.graph_objects as go
+import plotly
+from plotly.subplots import make_subplots
 
-df = pd.read_excel('Temporary Dataset -- VandyHacks Summer 2020.xlsx')
+
+excel_file = 'Facebook Page Q2.xlsx'
+df = pd.read_excel(excel_file)
+print(df)
+
+fig = make_subplots(rows=1, cols=2, column_widths=[0.5, 0.5])
+fig.add_trace(
+    go.Scatter(x=df['Date'] , y = df[ 'Daily Page Engaged Users'], mode='lines+markers', line_color="#ef5a41"),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(x=df['Date'] , y = df[ 'Weekly Page Engaged Users'], mode='lines+markers', line_color="#9467bd"),
+    row=1, col=2
+)
+fig.update_layout(
+autosize=False,
+    width=2000,
+    height=500,
+    margin=dict(
+        l=50,
+        r=50,
+        b=100,
+        t=100,
+        pad=20
+    ),
+    paper_bgcolor="LightSteelBlue",
+    title=go.layout.Title(
+        text="Facebook Page Quarter 2",
+        xref="paper",
+        x=0
+    ),
+    xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(
+            text="Daily Page Engaged Users",
+            font=dict(
+                family="Comissioner, sans-serif",
+                size=18,
+                color="#7f7f7f"
+            )
+        )
+    ),
+    yaxis=go.layout.YAxis(
+        title=go.layout.yaxis.Title(
+            text="Date",
+            font=dict(
+                family="Comissioner, sans-serif",
+                size=18,
+                color="#7f7f7f"
+            )
+        )
+    )
+)
+fig.show()
+
+
 
 layout = plot.Layout(
     title="Facebook"
 )
+
 
 fig1 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['Facebook Advertising'], mode='lines+markers', ))
 fig2 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['Facebook Reach'], mode='lines+markers', line_color="#ef5a41"))
@@ -34,13 +92,14 @@ fig7.add_trace(plot.Scatter(x=df['Date'], y=df['LinkedIn Reach'],
 fig7.add_trace(plot.Scatter(x=df['Date'], y=df['Email Marketing'],
                     mode='lines+markers', name='Email Marketing'))
 
+
 app = dash.Dash(__name__)
 app.layout = html.Div([
     html.Div([
-            html.H3('Summary of May 1 - 13, 2020'),
+            html.H3('Facebook Page Quarter 2'),
             dcc.Graph(
                 id='g7',
-                figure=fig7,
+                figure=data,
         )], className="heading bottom"),
 
     html.Div([
@@ -103,5 +162,5 @@ app.layout = html.Div([
     ], className="source")
 ],  className="container",)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
