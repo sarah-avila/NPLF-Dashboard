1c194f674cdde1cc0210a0b4b4c904e5276eb860
Sarah Avila <sttavila@gmail.com>
2020-11-06 15:08:04 -0800

    add sticky navbar and place vertical navbar

--- appi.py
+++ appi.py
@@ -21,6 +21,7 @@ fig4 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['Twitter Reach'], mode='
 fig5 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['LinkedIn Reach'], mode='lines+markers', line_color="#ffa15a"))
 fig6 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['Email Marketing'], mode='lines+markers', line_color="#1cd3f3"))
 fig7 = plot.Figure()
+
 fig7.add_trace(plot.Scatter(x=df['Date'], y=df['Facebook Advertising'],
                     mode='lines+markers',
                     name='Facebook Advertising'))
@@ -35,6 +36,7 @@ fig7.add_trace(plot.Scatter(x=df['Date'], y=df['LinkedIn Reach'],
 fig7.add_trace(plot.Scatter(x=df['Date'], y=df['Email Marketing'],
                     mode='lines+markers', name='Email Marketing'))
 
+# button group definitions
 vertical_navbar = dbc.ButtonGroup(
     [
         dbc.Button("Overview"),
@@ -47,6 +49,7 @@ vertical_navbar = dbc.ButtonGroup(
         
     ],
     vertical=True,
+    className="navbar-vertical",
 )
 
 horizontal_navbar = dbc.ButtonGroup(
@@ -60,78 +63,99 @@ horizontal_navbar = dbc.ButtonGroup(
         dbc.Button("Summary"),
         
     ],
+    className="navbar-horizontal",
 )
 
+# navbar definition
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Weekly", href="#"),
+                dbc.DropdownMenuItem("Monthly", href="#"),
+                dbc.DropdownMenuItem("Quarterly", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Overview",
+        ),
+        dbc.NavItem(dbc.NavLink("MoM", href="#")),
+        dbc.NavItem(dbc.NavLink("Summary", href="#")),
+    ],
+    brand="Nashville Public Library Foundation",
+    color="dark",
+    dark=True,
+    sticky="top",
+)
+
+# app and layout definition
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([
     vertical_navbar,
-    horizontal_navbar,
-    html.Div([
-            html.H3('Summary of May 1 - 13, 2020'),
-            dcc.Graph(
-                id='g7',
-                figure=fig7,
-        )], className="heading bottom"),
-
+    navbar,
     html.Div([
         html.Div([
-            html.H3('Facebook Advertising'),
-            dcc.Graph(
-                id='g1',
-                figure=fig1)], 
-        className="heading"),
+                html.H3('Summary of May 1 - 13, 2020'),
+                dcc.Graph(
+                    id='g7',
+                    figure=fig7,
+            )], className="heading top"),
 
         html.Div([
-            html.H3('Facebook Reach'),
-            dcc.Graph(
-                id='g2', figure=fig2)], 
-        className="heading"),
-    ], 
-    className="row top"),
+            html.Div([
+                html.H3('Facebook Advertising'),
+                dcc.Graph(
+                    id='g1',
+                    figure=fig1)], 
+            className="heading"),
 
-    html.Div([
-        html.Div([
-            html.H3('Google Analytics'),
-            dcc.Graph(
-                id='g3',
-                figure=fig3)],
-        className="heading"),
+            html.Div([
+                html.H3('Facebook Reach'),
+                dcc.Graph(
+                    id='g2', figure=fig2)], 
+            className="heading"),
+        ], 
+        className="row top"),
 
         html.Div([
-            html.H3('Twitter Reach'),
-            dcc.Graph(
-                id='g4', 
-                figure=fig4)], 
-                className="heading")],
+            html.Div([
+                html.H3('Google Analytics'),
+                dcc.Graph(
+                    id='g3',
+                    figure=fig3)],
+            className="heading"),
+
+            html.Div([
+                html.H3('Twitter Reach'),
+                dcc.Graph(
+                    id='g4', 
+                    figure=fig4)], 
+                    className="heading")],
+            className="row"),
+
+        html.Div([
+            html.Div([
+                html.H3('LinkedIn Reach'),
+                dcc.Graph(
+                    id='g5',
+                    figure=fig5)],
+            className="heading"),
+
+            html.Div([
+                html.H3('Email Marketing'),
+                dcc.Graph(
+                    id='g6', 
+                    figure=fig6)], 
+            className="heading")],
         className="row"),
 
-    html.Div([
         html.Div([
-            html.H3('LinkedIn Reach'),
-            dcc.Graph(
-                id='g5',
-                figure=fig5)],
-        className="heading"),
-
-        html.Div([
-            html.H3('Email Marketing'),
-            dcc.Graph(
-                id='g6', 
-                figure=fig6)], 
-        className="heading")],
-    className="row"),
-
-    # html.Div([
-    #         html.H3('Summary'),
-    #         dcc.Graph(
-    #             id='g7',
-    #             figure=fig7,
-    #     )], className="heading bottom"),
-
-    html.Div([
-        html.H5('Source: Nashville Public Library Foundation Official Records')
-    ], className="source")
-],  className="container",)
+            html.H5('Source: Nashville Public Library Foundation Official Records')
+        ], className="source")
+    ], 
+    className="container"),
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
