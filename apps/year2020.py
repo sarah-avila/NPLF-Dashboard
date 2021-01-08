af58c83bd4872467ed42e913e34a72c4f0c71397
Sarah Avila <sttavila@gmail.com>
2021-01-07 16:43:10 -0800

    page navigation works

--- apps/year2020.py
+++ apps/year2020.py
@@ -27,11 +27,13 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
+#layout = go.Layout()
+
 fig7 = make_subplots(rows=3, cols=1, subplot_titles=("Twitter", "Facebook", "Linkedin"))
-fig7.update_layout(height= 2500, width= 1700, title_text="Social Media Overview",)
+fig7.update_layout(height= 2300, width= 1300, title_text="Social Media Overview",)
 
 #Twitter
-df = pd.read_excel('NPLF Twitter 2021.xlsx')
+df = pd.read_excel('NPLF Twitter Q1andQ2.xlsx')
 
 fig1 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['impressions'], mode='lines+markers', ))
 fig2 = plot.Figure(data=plot.Scatter(x=df['Date'], y=df['engagement rate'], mode='lines+markers', line_color="#ef5a41"))
@@ -68,7 +70,7 @@ fig7.add_trace(go.Scatter(trace6,
 # layout = go.Layout(
 #     title="Facebook"
 # )
-df1 = pd.read_excel('FacebookPosts2021.xlsx')
+df1 = pd.read_excel('Facebook Posts Q1andQ2.xlsx')
 
 fig1 = go.Figure(data=go.Scatter(x=df1['Posted'], y=df1['Lifetime Post Total Reach'], mode='lines+markers', ))
 fig2 = go.Figure(data=go.Scatter(x=df1['Posted'], y=df1['Lifetime Post Total Impressions'], mode='lines+markers', line_color="#ef5a41"))
@@ -96,11 +98,11 @@ fig7.add_trace(go.Scatter(x=df1['Posted'], y=df1['Lifetime Matched Audience Targ
 
 #Linkedin
 #Data Frames and Sheet Names
-df2 = pd.read_excel('NPLF Linkedin 2021.xlsx', sheet_name = [0,1,2,3,4])
-df3 = pd.read_excel('NPLF Linkedin 2021.xlsx', sheet_name = 'Company size')
-df4 = pd.read_excel('NPLF Linkedin 2021.xlsx', sheet_name = 'Update metrics (aggregated)')
-df5 = pd.read_excel('NPLF Linkedin 2021.xlsx', sheet_name = 'Industry')
-df6 = pd.read_excel('NPLF Linkedin 2021.xlsx', sheet_name = 'Seniority')
+df2 = pd.read_excel('NPLF LinkedIn Q1.xlsx', sheet_name = [0,1,2,3,4])
+df3 = pd.read_excel('NPLF LinkedIn Q1.xlsx', sheet_name = 'Company size')
+df4 = pd.read_excel('NPLF LinkedIn Q1.xlsx', sheet_name = 'Update metrics (aggregated)')
+df5 = pd.read_excel('NPLF LinkedIn Q1.xlsx', sheet_name = 'Industry')
+df6 = pd.read_excel('NPLF LinkedIn Q1.xlsx', sheet_name = 'Seniority')
 
 fig1 = plot.Figure(data=plot.Scatter(y=df4["Date"], x=df4["Impressions (organic)"], mode='lines+markers', ))
 fig2 = plot.Figure(data=plot.Scatter(y=df4['Date'], x=df4['Impressions (sponsored)'], mode='lines+markers', line_color="#ef5a41"))
@@ -111,7 +113,7 @@ fig6 = plot.Figure(data=plot.Scatter(y=df4['Date'], x=df4['Reactions (organic)']
 fig8 = plot.Figure(data=plot.Scatter(y=df4['Date'], x=df4['Engagement rate (organic)'], mode='lines+markers', line_color="#F5A10E"))
 fig9 = plot.Figure(data=plot.Scatter(y=df4['Date'], x=df4['Engagement rate (sponsored)'], mode='lines+markers', line_color="#0EF596"))
 
-#fig9 = go.Figure()
+#Quarter 1 (april to june)
 
 fig7.add_trace(plot.Scatter(x=df4['Date'], y=df4['Impressions (organic)'],
                             mode='lines+markers',
@@ -130,8 +132,11 @@ fig7.add_trace(plot.Scatter(x=df4['Date'], y=df4['Engagement rate (organic)'],
                             mode='lines+markers', name='Engagement rate (organic)'), row = 3, col = 1)
 fig7.add_trace(plot.Scatter(x=df4['Date'], y=df4['Engagement rate (sponsored)'],
                             mode='lines+markers', name='Engagement rate (sponsored)'), row = 3, col = 1)
+                            
 
-fig7.show()
+
+
+#fig7.show()
 
 # default rangeslider/graph values
 min_value = '2020-01-01'
@@ -158,6 +163,20 @@ date_mark = {i: dates[i] for i in range(0, 12)}
 
 # navbar definition
 sticky_navbar = dbc.NavbarSimple(
+    children=[
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Years", header=True),
+                dbc.DropdownMenuItem("First", href='/apps/2020'),
+                dbc.DropdownMenuItem("First", href='/apps/2021'),
+                dbc.DropdownMenuItem("Second", href='/apps/2022'),
+                dbc.DropdownMenuItem("Third", href='/apps/2023'),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="Years",
+        ),
+    ],
     brand="Nashville Public Library Foundation",
     color="dark",
     dark=True,
@@ -189,9 +208,6 @@ vertical_navbar = dbc.ButtonGroup(
 
 # app and layout definition
 app.layout = html.Div([
-    sticky_navbar,
-    # badge,
-    # vertical_navbar,
     html.Div([
         # first graph -- Twitter
         html.Div([
@@ -201,7 +217,7 @@ app.layout = html.Div([
                 figure=fig7,
             )], className="heading"),
 
-        # range slider with input boxes
+        # first range slider with input boxes
         html.Div([
             html.Label("Time Period"),
         ], style={"fontSize" : "20px", "marginTop" : "30px"}),
@@ -210,11 +226,11 @@ app.layout = html.Div([
             dbc.FormGroup(
             [
                 dbc.Label("Minimum Date"),
-                dbc.Input(id="min-input", placeholder=min_value, type="text", value=min_value),
+                dbc.Input(id="min-input-one", placeholder=min_value, type="text", value=min_value),
                 # dbc.FormText("yyyy-mm-dd"),
             ]),
             dcc.RangeSlider(
-                id='slider',
+                id='slider-one',
                 marks=date_mark,
                 min=0,
                 max=11,
@@ -224,10 +240,10 @@ app.layout = html.Div([
              dbc.FormGroup(
             [
                 dbc.Label("Maximum Date"),
-                dbc.Input(id="max-input", placeholder=max_value, type="text", value=max_value),
+                dbc.Input(id="max-input-one", placeholder=max_value, type="text", value=max_value),
                 # dbc.FormText("yyyy-mm-dd"),
             ]),
-            dbc.Button("Generate Graph", id="generate-button", className="mr-2")
+            dbc.Button("Generate Graph", id="generate-button-one", className="mr-2")
         ], className="rangeSlider"),
 
         # second graph -- Facebook
@@ -237,63 +253,40 @@ app.layout = html.Div([
                 figure=fig8,
             )], className="heading"),
 
+        # second range slider with input boxes
+        html.Div([
+            html.Label("Time Period"),
+        ], style={"fontSize" : "20px", "marginTop" : "30px"}),
+        html.Div(
+        [
+            dbc.FormGroup(
+            [
+                dbc.Label("Minimum Date"),
+                dbc.Input(id="min-input-two", placeholder=min_value, type="text", value=min_value),
+                # dbc.FormText("yyyy-mm-dd"),
+            ]),
+            dcc.RangeSlider(
+                id='slider-two',
+                marks=date_mark,
+                min=0,
+                max=11,
+                value=[0, 11],
+                allowCross=False
+            ),
+             dbc.FormGroup(
+            [
+                dbc.Label("Maximum Date"),
+                dbc.Input(id="max-input-two", placeholder=max_value, type="text", value=max_value),
+                # dbc.FormText("yyyy-mm-dd"),
+            ]),
+            dbc.Button("Generate Graph", id="generate-button-two", className="mr-2")
+        ], className="rangeSlider"),
+
         html.Div([
             html.H5('Source: Nashville Public Library Foundation Official Records')
         ], className="source")
     ]),
 ])
-
-@app.callback(Output(component_id='slider', component_property='marks'), [Input('slider', 'value'), Input("generate-button", "n_clicks"), Input("min-input", "value"), Input("max-input", "value")])
-def on_button_click(X, n, minValue, maxValue):
-    global date_mark
-    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-
-    if 'generate-button' in changed_id:
-        min_value = minValue
-        max_value = maxValue
-        new_date_mark = set_rangeslider(minValue, maxValue)[0]
-        date_mark = new_date_mark
-        return new_date_mark
-    else:
-        return date_mark
-
-
-# Step 5. Add callback functions
-@app.callback(Output('g7', 'figure'), [Input('slider', 'value'), Input("generate-button", "n_clicks"), Input('slider', 'marks')])
-def update_graph(X, n, dates):
-    print("X: ", X)
-    print("n: ", n)
-    print("dates: ", dates)
-    dates = list(dates.values())
-    print("dates as list", dates)
-
-    df2 = df[(df.Date >= dates[X[0]]) & (df.Date <= dates[X[1]])]
-    trace_1 = go.Scatter(x=df2.Date, y=df2['impressions'],
-                        name='impressions',
-                        line=dict(width=2,
-                                    color='#00cc96'))
-    trace_2 = go.Scatter(x=df2.Date, y=df2['engagement rate'],
-                        name='engagement rate',
-                        line=dict(width=2,
-                                    color='#FF5733'))
-    trace_3 = go.Scatter(x=df2.Date, y=df2['detail expands'],
-                        name='detail expands',
-                        line=dict(width=2,
-                                    color='#D7BDE2'))
-    trace_4 = go.Scatter(x=df2.Date, y=df2['likes'],
-                        name='likes',
-                        line=dict(width=2,
-                                    color='#9467bd'))
-    trace_5 = go.Scatter(x=df2.Date, y=df2['media views'],
-                        name='media views',
-                        line=dict(width=2,
-                                    color='#ffa15a'))
-    trace_6 = go.Scatter(x=df2.Date, y=df2['media engagements'],
-                        name='media engagements',
-                        line=dict(width=2,
-                                    color='#1cd3f3'))
-    fig = go.Figure(data=[trace_1, trace_2, trace_3, trace_4, trace_5, trace_6], layout=layout)
-    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
